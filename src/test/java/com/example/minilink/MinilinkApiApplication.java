c2665b86eab5a19c2bf1a5db831a923ae9aaec75
José Lueslefe <joselueslefelima@gmail.com>
2026-04-21 20:33:47 -0300

    Atualizar o MinilinkApiApplication.java

--- src/test/java/com/example/minilink/MinilinkApiApplication.java
+++ src/test/java/com/example/minilink/MinilinkApiApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public classv MinilinkApiApplication {
+public class MinilinkApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MinilinkApiApplication.class, args);
